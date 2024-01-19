5a78b26a6140ef0fd77f303cfd54bccccbd93d47
vpdiongzon <vpdiongzon@gmail.com>
2024-01-19 15:02:16 +0800

    initial form in login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -1,3 +1,5 @@
+import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+
 import Image from '@/assets/login-logo.svg';
 
 import { Layout } from '../components/Layout';
@@ -6,6 +8,12 @@ export const Login = () => {
   return (
     <Layout>
       <img src={Image} />
+
+      <FormControl>
+        <FormLabel>
+          <Input type="email" />
+        </FormLabel>
+      </FormControl>
 
       <h1>This is Login</h1>
     </Layout>
