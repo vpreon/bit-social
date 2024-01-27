148809da55784d64327a810cae09b1113a2f7eab
vpdiongzon <vpdiongzon@gmail.com>
2024-01-27 11:04:43 +0800

    initial layout of posts

--- src/features/home/pages/Home.tsx
+++ src/features/home/pages/Home.tsx
@@ -1,14 +1,30 @@
-// import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
+
+import { Posts } from '../components/posts';
 
 export const Home = () => {
+  const properties = [
+    {
+      imageUrl: 'https://bit.ly/2Z4KKcF',
+      imageAlt: 'Rear view of modern home with pool',
+      beds: 3,
+      baths: 2,
+      title: 'Modern home in city center in the heart of historic Los Angeles',
+      formattedPrice: '$1,900.00',
+      reviewCount: 34,
+      rating: 4,
+    },
+  ];
+
   return (
-    <div id="content">
-      <header>Header</header>
-      <main>Main</main>
-      <section>Section</section>
-      <aside>Aside</aside>
-      <nav>Nav</nav>
-      <footer>Footer</footer>
-    </div>
+    <Grid h="100%" templateColumns="repeat(12, 1fr)">
+      <GridItem colSpan={2}>left</GridItem>
+      <GridItem colSpan={8}>
+        {properties.map((item, key) => (
+          <Posts key={key} {...item} />
+        ))}
+      </GridItem>
+      <GridItem colSpan={2}>right</GridItem>
+    </Grid>
   );
 };
